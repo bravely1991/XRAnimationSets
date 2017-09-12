c711765febba456fc5b0b98d38508ab78ce6d0bc
bravely1991 <bravely1991@icloud.com>
2017-09-12 17:05:02 +0800

    环形多项展开饼图

--- XRAnnularPieView/IndexTableViewController.m
+++ XRAnnularPieView/IndexTableViewController.m
@@ -17,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"饼图";
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
